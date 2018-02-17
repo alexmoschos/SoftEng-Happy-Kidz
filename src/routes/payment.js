e3a5b22a52c55456fe69a01cdedff43c70409a39
Kalomoiros Alexandros <kalomiros7@gmail.com>
2018-02-17 21:28:23 +0200

    ROUTES/PAYMENT.JS

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -27,24 +27,20 @@ router.get('/', function (req, res) {
 	
 	/* Retrieve item to buy */
 	let userSession = req.session;
-	if (userSession){
-		if (userSession.cart){
-			let item = userSession.cart;
-			console.log(item);
-			switch (item.type) {
-				case 'ticket':
-					res.render('payment', {description: 'Εισιτήριο ' , amount: 10});
-					break;
-				case 'membership':
-					res.render('payment', {description: availMemberships[item.tier] , amount: availMemberships[item.tier]});
-					break;
-				default:
-					console.log('something definitely went wrong');
-					res.redirect('/');
-					break;
-			}
-		} else {
-			res.redirect('/');
+	if (userSession.cart){
+		let item = userSession.cart;
+		console.log(item);
+		switch (item.type) {
+			case 'ticket':
+				res.render('payment', {description: 'Εισιτήριο ' , amount: 10});
+				break;
+			case 'membership':
+				res.render('payment', {description: availMemberships[item.tier] , amount: availMemberships[item.tier]});
+				break;
+			default:
+				console.log('something definitely went wrong');
+				res.redirect('/');
+				break;
 		}
 	} else {
 		res.redirect('/');
@@ -53,33 +49,23 @@ router.get('/', function (req, res) {
 
 router.post('/events/:id', auth.isUserParent, function (req, res) {
 	/* Add ticket for event to cart */
-	console.log('Adding ticket to cart - session ' + req.session );
-	if (req.session && (req.session.passport.user.type === 'parent')){
-		req.session.cart = {
-			type: 'ticket',
-			eventId: req.params.id,
-			quantity: req.body.quantity
-		}
-		console.log(req.session.cart);
+	console.log('Adding ticket to cart - session ' + req.user.type );
+	req.session.cart = {
+		type: 'ticket',
+		eventId: req.params.id,
+		quantity: req.body.quantity
 	}
-	else {
-		res.redirect('/login');
-	}
+	res.redirect('/payment');
 });
 
 router.post('/membership/:id',auth.isUserParent, function (req, res) {
 	/* Add ticket for event to cart */
 	console.log('Adding membership to cart - session ' + req.user.type );
-	if (req.session && (req.user.type === 'parent')){
-		req.session.cart = {
-			type: 'membership',
-			tier: req.params.tier
-		}
-		res.redirect('/payment');
+	req.session.cart = {
+		type: 'membership',
+		tier: req.params.tier
 	}
-	else {
-		res.redirect('/login');
-	}
+	res.redirect('/payment');
 });
 
 module.exports = router;
