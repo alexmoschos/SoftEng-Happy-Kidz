adf22e3954f86423d54f799182c6299eb49b44a0
Grigoris Velegkas <gvelegkas41@gmail.com>
2018-02-18 19:41:28 +0200

    major admin changes

---
+++ src/models/seed.js
@@ -0,0 +1,116 @@
+const conf = require('../config.js');
+
+function seedDatabase(db) {
+    db.Parent.bulkCreate(
+            [{
+                    parentId: 1,
+                    name: 'Παναγόρης Βελένας',
+                    email: 'velenas@gmail.com',
+                    password: '123456',
+                    wallet: 0,
+                    mailNotifications: false
+                },
+                {
+                    parentId: 2,
+                    name: 'Κυριάκος Μοσχονάς',
+                    email: 'moschokyr@gmail.com',
+                    password: '123456',
+                    wallet: 0,
+                    mailNotifications: false
+                }
+            ])
+        .then((succ) => db.Organizer.bulkCreate(
+            [{
+                    organizerId: 1,
+                    name: 'lkajfklajfklajfklajfkljaklfjalkjfklasjfkkjfjfΒελεγκοιβεντς',
+                    email: 'velegkevents@velegkas.com',
+                    password: '123456',
+                    description: '',
+                    phone: '2105874658',
+                    webpage: 'www.thevelegk.gr',
+                    avgRating: 4,
+                    avatar: '',
+                    isVerified: false,
+                    documents: ''
+                },
+                {
+                    organizerId: 2,
+                    name: 'Κοφι Εντερπραιζις',
+                    email: 'theman@otenet.gr',
+                    password: '123456',
+                    description: '',
+                    phone: '2104845145',
+                    webpage: 'www.theman.gr',
+                    avgRating: 1,
+                    avatar: '',
+                    isVerified: true,
+                    documents: ''
+                }
+            ]))
+        .then((succ) => db.Admin.bulkCreate(
+            [{
+                    username: 'admin',
+                    password: 'admin',
+                    email: 'admin@admin.com'
+                },
+                {
+                    username: 'dev',
+                    password: 'password',
+                    email: 'dev@admin.com'
+                }
+            ]))
+        .then((succ) => db.Event.bulkCreate(
+            [{
+                    eventId: 1,
+                    organizerId: 1,
+                    title: 'Παρτυ με τον Βελεγκα χωρις τον Βελεγκα',
+                    startTime: Math.floor(Date.now() / 1000),
+                    endTime: Math.floor(Date.now() / 1000 + 3600),
+                    description: '',
+                    categoryName: 'Αθλητισμός',
+                    geoLon: '37.988930',
+                    geoLat: '23.764727',
+                    geoAddress: 'Ευρυτανίας 56, ΤΚ15451, Αθήνα',
+                    ticketPrice: 15,
+                    discount: 5,
+                    ticketCount: 100,
+                    minAge: 10,
+                    maxAge: 18,
+                    pictures: 0,
+                    clickNumber: 0,
+                    isVerified: false
+                },
+                {
+                    eventId: 2,
+                    organizerId: 2,
+                    title: ' με τον Βελεγκα χωρις τον Βελεγκα',
+                    startTime: Math.floor(Date.now() / 1000),
+                    endTime: Math.floor(Date.now() / 1000 + 3600),
+                    description: '',
+                    categoryName: 'Αθλητισμός',
+                    geoLon: '37.988930',
+                    geoLat: '23.764727',
+                    geoAddress: 'Ευρυτανίας 56, ΤΚ15451, Αθήνα',
+                    ticketPrice: 15,
+                    discount: 5,
+                    ticketCount: 100,
+                    minAge: 10,
+                    maxAge: 18,
+                    pictures: 0,
+                    clickNumber: 0,
+                    isVerified: false
+                }
+            ]))
+        .catch((err) => console.log(err));
+    // for each supported category make sure, we have it in the categories table
+    conf.supportedCategories.forEach(function(category, idx) {
+        db.Categories.findOne({ where: { categoryName: category } }).then(res => {
+            if (!res) {
+                // this category has to be added.
+                db.Categories.create({ categoryName: category }).catch(err => { console.log(err); });
+            }
+        }).catch(err => { console.log(err); });
+    });
+}
+
+module.exports = { seedDatabase: seedDatabase };
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../models/db');
+var mail = require('../apis/mail');
 
 
 router.get('/', function(req, res, next) {
+    
+
 
     db.Parent.findAll()
     .then( (parents) => parents.forEach( (parent) => console.log(parent.name) )
@@ -16,6 +19,8 @@ router.get('/', function(req, res, next) {
             isVerified: false
         }
     });
+    /* test gia na doume an itan swsto to accept */
+    //var promise3 = db.Event.findAll();
 
     var obj;
 
@@ -26,5 +31,86 @@ router.get('/', function(req, res, next) {
     });
 
 });
+
+// ******************** Reset passwords, isws meta mpei san put sto antistoixo object //
+router.put('/parent/:parentId/reset', function(req, res, next) {
+
+    db.Parent.findById(req.params.parentId)
+    .then( (parent) =>{
+        if (parent) {
+            console.log(parent.name + " password reset");
+            //edw prepei na kanoume reset kai na steiloume mail
+            res.redirect("/admin");
+        }
+        else {
+            res.send("no such parent!");
+        }
+
+    });
+});
+
+router.put('/provider/:providerId/reset', function(req, res, next) {
+
+    db.Organizer.findById(req.params.providerId)
+    .then( (provider) =>{
+        if (provider) {
+            console.log(provider.name + " password reset");
+            //edw prepei na kanoume reset kai na steiloume mail
+            res.redirect("/admin");
+
+        }
+        else {
+            res.send("no such provider!");
+        }
+
+    });
+});
+
+
+// ******************** View Events, Providers //
+
+
+router.get('/events/:eventId', function(req, res, next) {
+
+    db.Event.findById(req.params.eventId)
+    .then( (event) => {
+        if (event) {
+            db.Organizer.findById(event.organizerId)
+            .then( (provider) => {
+                if (event && event.isVerified === false) {
+                    console.log(event.name + " found event");
+                    event.provider = provider;
+                    console.log(event);
+                    obj = event;
+                    res.render("adminEvent", {obj});
+                    //res.redirect('/admin');
+                }
+                else {
+                    res.send("no such event!");
+                }
+            });
+        }
+        else {
+            res.send("no such event!");
+        }
+    });
+});
+
+router.get('/provider/:providerId', function(req, res, next) {
+
+    db.Organizer.findById(id)
+    .then( (provider) => {
+        if (provider) {
+            console.log(provider.name + " found provider");
+            res.render("adminProvider", provider);
+        }
+        else {
+            res.send("no such provider!");
+        }
+
+    });
+});
+
+
 
 module.exports = router;
