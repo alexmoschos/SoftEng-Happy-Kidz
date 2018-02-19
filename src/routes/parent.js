a9f7b46a749a2ded250983250fab07d9d86c56e5
Kyriakos Lotidis <klotidis10@gmail.com>
2018-02-19 21:25:08 +0200

    added parent page

--- src/routes/parent.js
+++ src/routes/parent.js
@@ -1,11 +1,85 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../models/db');
+var bcrypt = require('bcrypt');
 
 /* GET parent profile. */
-router.get('/:id', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/:parentId', function(req, res, next) {
+	db.Parent.findById(req.params.parentId)
+	.then( (parent) => {
+		db.Organizer.findAll({
+			include: [{
+			  model: db.Event,
+			  required: true
+			}],
+			// where: { organizerId: 4 }
+		  })
+		  .then( tickets => { 
+			console.log(tickets);
+			obj = {
+				user: req.user,
+				id: parent.parentId ,
+				name: parent.name,
+				category:"Gold",
+				expiryDate: "12-12-12",
+				points: parent.wallet,
+				email: parent.email,
+				bought: tickets
+			};
+			res.render('parent', obj);
+		})
+	});
 });
+
+router.get('/:parentId/settings', function(req, res){
+	db.Parent.findById(req.params.parentId)
+	.then( (parent) => { 
+		obj = {
+			user: req.user,
+			errors: [],
+			id: parent.parentId,
+			name: parent.name,
+			email: parent.email
+		}
+		res.render('parentSettings', obj)
+})
+});
+
+
+router.post('/:parentId/settings', function(req, res){
+	db.Parent.findById(req.params.parentId)
+	.then( (parent) => { 
+		var temp = bcrypt.compareSync(req.body.oldPassword, parent.password);
+		if(!temp){
+			req.assert('oldPassword', 'Λάθος Κωδικός').equals(true);			
+		}
+		req.assert('email', 'A valid email is required').isEmail(); 
+		req.assert('newPassword', 'passwords must be at least 8 chars long and contain one number')
+			.isLength({ min: 8 })
+			.matches(/\d/);
+		req.assert('newPasswordAgain', 'Passwords do not match').equals(req.body.newPassword);
+		var errors = req.validationErrors();
+		if( !errors){
+			parent.update({email: req.body.email})
+			.then ( (succ) => {
+				return succ.update({password: bcrypt.hashSync(req.body.newPassword,10)});
+			})
+			.then( (succe) => res.redirect("/parent/" + req.params.parentId));
+		}
+		else{
+			obj = {
+				user: req.user,
+				errors: errors,
+				id: parent.parentId,
+				name: parent.name,
+				email: parent.email
+			};
+			res.render('parentSettings', obj);
+		}
+	})
+});
+
+
 
 router.delete('/:parentId', function(req, res){
 	db.Parent.findById(req.params.parentId)
@@ -17,6 +91,6 @@ router.delete('/:parentId', function(req, res){
 		}
 	}  )
 	.then( (succeed) => res.redirect("/admin") );
-})
+});
 
 module.exports = router;
