785095d620bb652bfe0eb20c638f86a292343f1c
Kyriakos Lotidis <klotidis10@gmail.com>
2018-02-20 19:03:00 +0200

    added authentication in parent profile

--- src/routes/parent.js
+++ src/routes/parent.js
@@ -2,10 +2,10 @@ var express = require('express');
 var router = express.Router();
 var db = require('../models/db');
 var auth = require('../apis/authentication');
-
+var bcrypt = require('bcrypt');
 
 /* GET parent profile. */
-router.get('/:parentId', function(req, res, next) {
+router.get('/:parentId', auth.isUserParentId, function(req, res, next) {
 	db.Parent.findById(req.params.parentId)
 	.then( (parent) => {
 		db.BoughtTickets.findAll({
@@ -18,16 +18,30 @@ router.get('/:parentId', function(req, res, next) {
 		.then( tickets => { 
 			db.Membership.findById(req.params.parentId)
 			.then( member => {
-				obj = {
-					user: req.user,
-					id: parent.parentId ,
-					name: parent.name,
-					category: member.membershipTier,
-					expiryDate: member.expiryDate,
-					points: parent.wallet,
-					email: parent.email,
-					bought: tickets
-				};
+				if(member != null){
+					obj = {
+						user: req.user,
+						id: parent.parentId ,
+						name: parent.name,
+						category: member.membershipTier,
+						expiryDate: member.expiryDate,
+						points: parent.wallet,
+						email: parent.email,
+						bought: tickets
+					};
+				}
+				else{
+					obj = {
+						user: req.user,
+						id: parent.parentId ,
+						name: parent.name,
+						category: 0,
+						expiryDate: 0,
+						points: parent.wallet,
+						email: parent.email,
+						bought: tickets
+					};
+				}
 				res.render('parent', obj);
 			})	
 			// console.log(tickets);
@@ -35,7 +49,7 @@ router.get('/:parentId', function(req, res, next) {
 	});
 });
 
-router.get('/:parentId/settings', function(req, res){
+router.get('/:parentId/settings', auth.isUserParentId, function(req, res){
 	db.Parent.findById(req.params.parentId)
 	.then( (parent) => { 
 		obj = {
@@ -50,36 +64,42 @@ router.get('/:parentId/settings', function(req, res){
 });
 
 
-router.post('/:parentId/settings', function(req, res){
-	db.Parent.findById(req.params.parentId)
-	.then( (parent) => { 
-		var temp = bcrypt.compareSync(req.body.oldPassword, parent.password);
-		if(!temp){
-			req.assert('oldPassword', 'Λάθος Κωδικός').equals(true);			
-		}
-		req.assert('email', 'A valid email is required').isEmail(); 
-		req.assert('newPassword', 'passwords must be at least 8 chars long and contain one number')
-			.isLength({ min: 8 })
-			.matches(/\d/);
-		req.assert('newPasswordAgain', 'Passwords do not match').equals(req.body.newPassword);
-		var errors = req.validationErrors();
-		if( !errors){
-			parent.update({email: req.body.email})
-			.then ( (succ) => {
-				return succ.update({password: bcrypt.hashSync(req.body.newPassword,10)});
-			})
-			.then( (succe) => res.redirect("/parent/" + req.params.parentId));
-		}
-		else{
-			obj = {
-				user: req.user,
-				errors: errors,
-				id: parent.parentId,
-				name: parent.name,
-				email: parent.email
-			};
-			res.render('parentSettings', obj);
-		}
+router.post('/:parentId/settings', auth.isUserParentId,function(req, res){
+	var find = auth.findUserByEmail(req.body.email, function(result) {
+		db.Parent.findById(req.params.parentId)
+		.then( (parent) => { 
+			if (result && req.body.email !== parent.email){
+				console.log('$$$$$$$$$$$$$$$$$$$$$');
+				req.assert('email', 'Υπάρχει ήδη χρήστης με αυτό το email').equals(true);	
+			}
+			var temp = bcrypt.compareSync(req.body.oldPassword, parent.password);
+			if(!temp){
+				req.assert('oldPassword', 'Λάθος Κωδικός').equals(true);			
+			}
+			req.assert('email', 'A valid email is required').isEmail(); 
+			req.assert('newPassword', 'passwords must be at least 8 chars long and contain one number')
+				.isLength({ min: 8 })
+				.matches(/\d/);
+			req.assert('newPasswordAgain', 'Passwords do not match').equals(req.body.newPassword);
+			var errors = req.validationErrors();
+			if( !errors){
+				parent.update({email: req.body.email})
+				.then ( (succ) => {
+					return succ.update({password: bcrypt.hashSync(req.body.newPassword,10)});
+				})
+				.then( (succe) => res.redirect("/parent/" + req.params.parentId));
+			}
+			else{
+				obj = {
+					user: req.user,
+					errors: errors,
+					id: parent.parentId,
+					name: parent.name,
+					email: parent.email
+				};
+				res.render('parentSettings', obj);
+			}
+		})
 	})
 });
 
