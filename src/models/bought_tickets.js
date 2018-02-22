769ced0fdc92e0aa24a178500f46c3e5f195f5b6
Yannis Zarkadas <yanniszarkadas@gmail.com>
2018-02-22 15:06:59 +0200

    Bought_Tickets model minor fix

--- src/models/bought_tickets.js
+++ src/models/bought_tickets.js
@@ -15,7 +15,7 @@ module.exports = function(sequelize, DataTypes) {
             allowNull: false
         },
         transactionId: {
-            type: DataTypes.INTEGER,
+            type: DataTypes.STRING,
             allowNull: false
         },
         startTime: {
