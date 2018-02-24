b4399709f4de95b64b5293aaab4e69da543d0976
vardis <el13028@central.ntua.gr>
2018-02-24 18:25:39 +0200

    merge

--- src/routes/membership.js
+++ src/routes/membership.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next){
-		res.render('membership',{});
+		res.render('membership',{user: req.user});
 });
 
 
